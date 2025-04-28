4ba757a32fff9549b88ca7c7b6ba9e41d318b077
Ruslan <ruslankhakimullin@gmail.com>
2025-04-28 17:27:21 +0300

    ex00 workig

--- ex00/ex00.cpp
+++ ex00/ex00.cpp
@@ -6,20 +6,27 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:21:15 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/04/28 16:39:30 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:25:45 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+int main (int ac, char **av)
+{    
+    if (ac < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
 
-char    my_toupper(char ch)
-{
-    std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(ch)));
-}
-int main()
-{
-    char ch{};
-    std::cin >> ch;
-    
+    for (int i = 1; i < ac; i++)
+    {
+        for (int j = 0; i < av[i][j]; j++)
+        {
+            std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(av[i][j])));
+        }
+    }
+    std::cout << "\n" << std::flush;
+    return (0);
 }
