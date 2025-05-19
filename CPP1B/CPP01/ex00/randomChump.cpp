30e682ec63fff66519a86cdbe65655314ab14e4f
Ruslan <ruslankhakimullin@gmail.com>
2025-05-19 09:17:19 +0300

    WIP ex04

--- CPP1B/CPP01/ex00/randomChump.cpp
+++ CPP1B/CPP01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:16:56 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/14 11:06:51 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/19 08:35:50 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void randomChump(std::string name)
 {
-    /* Zombie a =  */Zombie a(name);
+    Zombie a(name);
     a.announce();
 }
--- CPP1B/CPP01/ex04/main.cpp
+++ CPP1B/CPP01/ex04/main.cpp
@@ -6,34 +6,42 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 10:45:02 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/16 16:21:42 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/19 09:16:49 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-std::string replaceAll(const std::string& text, const std::string& s1, const std::string& s2) {
-    std::string result;
-    size_t lastPos = 0;
-    size_t pos = 0;
-    // Find each occurrence of s1
-    while ((pos = text.find(s1, lastPos)) != std::string::npos) {
-        std::cout << "pos: " << pos << " lastPos: " << lastPos << std::endl;
-        // Copy text before the match
-        result += text.substr(lastPos, pos - lastPos);
-        // Add replacement
-        result += s2;
-        lastPos = pos + s1.length();
-    }
-    std::cout << "pos: " << pos << " lastPos: " << lastPos << std::endl;
-    // Copy remaining text
-    result += text.substr(lastPos);
-    return result;
+std::string replaceAll(const std::string &text, const std::string &s1, const std::string &s2)
+{
+	std::string result;
+	size_t match_pos = 0;
+	size_t last_match_pos = 0;
+	
+	while ((match_pos = text.find(s1, last_match_pos)) != std::string::npos)
+	{
+		//Copying the text before the match
+		result += text.substr(last_match_pos, match_pos - last_match_pos);
+		//Adding replacement
+		result += s2;
+		last_match_pos = match_pos + s1.length();
+	}
+	//Copying remaining text
+	result = text.substr(last_match_pos);
+	return (result);
 }
 
-int main(int ac, char *av[]) {
-    if (ac != 4) return 1;
-    std::cout << replaceAll("add some text some fun", av[2], av[3]) << std::endl;
-    return 0;
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cerr << "Usage " << av[0] << " <filename> <s1> <s2>" << std::endl;
+		return (1); 
+	}
+
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3]
 }
