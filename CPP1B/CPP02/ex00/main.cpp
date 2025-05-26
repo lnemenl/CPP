e3ef3e0cd0dc8e3c8dc3ab9e3cf5a1de094d6c00
Ruslan <ruslankhakimullin@gmail.com>
2025-05-26 11:43:17 +0300

    WIP c02

--- CPP1B/CPP02/ex00/main.cpp
+++ CPP1B/CPP02/ex00/main.cpp
@@ -5,35 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 14:13:09 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/26 11:37:19 by rkhakimu         ###   ########.fr       */
+/*   Created: 2025/05/26 11:38:41 by rkhakimu          #+#    #+#             */
+/*   Updated: 2025/05/26 11:38:42 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanA.hpp"
-#include "HumanB.hpp"
-
-
-int main()
-{
-    {
-        Weapon club = Weapon("crude spiked club");
-        HumanA bob("Bob", club);
-        bob.attack();
-        club.setType("some other type of club");
-        bob.attack();
-    }
-    {
-        Weapon club = Weapon("crude spiked club");
-        HumanB jim("Jim");
-        jim.setWeapon(club);
-        jim.attack();
-        club.setType("some other type of club");
-        jim.attack();
-    }
-    {
-        HumanB unarmed("Alice");
-        unarmed.attack();
-    }
-    return (0);
-}
--- CPP1B/CPP02/ex01/Fixed.cpp
+++ CPP1B/CPP02/ex01/Fixed.cpp
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 14:13:05 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/22 16:57:47 by rkhakimu         ###   ########.fr       */
+/*   Created: 2025/05/26 11:40:24 by rkhakimu          #+#    #+#             */
+/*   Updated: 2025/05/26 11:40:25 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HumanB.hpp"
-
-HumanB::HumanB(std::string name) : name(name), weapon(nullptr) {}
-
-void HumanB::setWeapon(Weapon& weapon)
-{
-	this->weapon = &weapon;
-}
-
-void HumanB::attack() const
-{
-	if (weapon)
-	{
-		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
-	}
-	else
-	{
-		std::cout << name << " is unarmed and cannot attack" << std::endl;
-	}
-}
