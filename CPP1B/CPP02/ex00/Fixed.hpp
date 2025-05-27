44ece27fb3c88ec84dd1fab9852e098748be71d0
Ruslan <ruslankhakimullin@gmail.com>
2025-05-27 16:57:32 +0300

    WIP CPP02

--- CPP1B/CPP02/ex00/Fixed.hpp
+++ CPP1B/CPP02/ex00/Fixed.hpp
@@ -6,12 +6,24 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 11:39:12 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/26 11:49:24 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:46:41 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
-#include <string>
 
+class Fixed
+{
+    private:
+        int _value;
+        static const int fract_bits;
+        
+    public:
+        Fixed();
+        Fixed (const Fixed& obj);
+        Fixed& operator=(const Fixed& obj);
+        ~Fixed();
+               
+}
