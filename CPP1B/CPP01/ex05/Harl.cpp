4834364d24042185ce026db95a2c9a72d17d4e5f
Ruslan <ruslankhakimullin@gmail.com>
2025-05-27 14:54:04 +0300

    Stylistic changes to ex05

--- CPP1B/CPP01/ex05/Harl.cpp
+++ CPP1B/CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 11:23:12 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/27 14:43:11 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/27 14:53:15 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,32 @@
 
 void Harl::debug()
 {
-	std::cout	<< "[DEBUG] I love having extra bacon for my "
+	std::cout	<< "[DEBUG]" << std::endl
+				<< "I love having extra bacon for my "
 				<< "7XL-double-cheese-triple-pickle-special-ketchup burger. "
 				<< "I really do!" << std::endl;
 }
 
 void Harl::info()
 {
-	std::cout	<< "[INFO] I cannot believe adding extra bacon costs more money. "
+	std::cout	<< "[INFO]" << std::endl
+				<< "I cannot believe adding extra bacon costs more money. "
 				<< "You didn't put enough bacon in my burger! "
 				<< "If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout	<< "[WARNING] I think I deserve to have some extra bacon for free. "
+	std::cout	<< "[WARNING]" << std::endl
+				<< "I think I deserve to have some extra bacon for free. "
 				<< "I've been coming for years whereas you started "
 				<< "working here since last month." << std::endl;
 }
 
 void Harl::error()
 {
-	std::cout	<< "[ERROR] This is unacceptable! I want to speak to the manager now."
+	std::cout	<< "[ERROR]" << std::endl
+				<< "This is unacceptable! I want to speak to the manager now."
 				<< std::endl;
 }
 
@@ -59,5 +63,6 @@ void Harl::complain(std::string level)
 			return ;
 		}
 	}
-	std::cout << "[INVALID] Probably complaining about insignificant problems." << std::endl;
+	std::cout 	<< "[INVALID]" << std::endl
+				<< "Probably complaining about insignificant problems." << std::endl;
 }
