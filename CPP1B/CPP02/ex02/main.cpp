18049045c3df299d75d49b4cd8c2aaf6fddc91f4
Rus <ruslankhakimullin@gmail.com>
2025-05-31 19:44:48 +0300

    Added more cout statements for clarity and visibility to CPP02/ex02

--- CPP1B/CPP02/ex02/main.cpp
+++ CPP1B/CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 11:40:59 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/31 16:10:11 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:27:52 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << "a   is: " << a << std::endl;
+	std::cout << "++a is: " << ++a << std::endl;
+	std::cout << "a   is: " << a << std::endl;
+	std::cout << "a++ is: " << a++ << std::endl;
+	std::cout << "a   is: " << a << std::endl;
+	std::cout << "b   is: " << b << std::endl;
+	std::cout << "max is: " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min is: " << Fixed::min( a, b ) << std::endl;
 	return 0;
 }
