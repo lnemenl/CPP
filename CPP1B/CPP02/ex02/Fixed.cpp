a5cd202ee15ae62082c5e8402e1860a2356d3cb6
Ruslan <ruslankhakimullin@gmail.com>
2025-05-31 15:18:43 +0300

    CPP02 ex02 ready

--- CPP1B/CPP02/ex02/Fixed.cpp
+++ CPP1B/CPP02/ex02/Fixed.cpp
@@ -6,7 +6,142 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 11:41:09 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/26 11:41:10 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/31 15:18:17 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <Fixed.hpp>
+
+//Orthodox Canonical form
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& obj)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = obj;
+}
+
+Fixed& Fixed::operator=(const Fixed& obj)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &obj)
+    {
+        _value = obj._value;
+    }
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+
+//Comparison operators
+bool	Fixed::operator>(const Fixed& obj) const
+{
+    return _value > obj._value;
+}
+
+bool	Fixed::operator<(const Fixed& obj) const
+{
+    return _value < obj._value;
+}
+
+bool	Fixed::operator>=(const Fixed& obj) const
+{
+    return _value >= obj._value;
+}
+
+bool	Fixed::operator<=(const Fixed& obj) const
+{
+    return _value <= obj._value;
+}
+
+bool	Fixed::operator==(const Fixed& obj) const
+{
+    return _value == obj._value;
+}
+
+bool	Fixed::operator!=(const Fixed& obj) const
+{
+    return !(_value == obj._value);
+}
+
+
+//Arithmetic operators
+Fixed& Fixed::operator+(const Fixed& obj) const
+{
+    Fixed sum;
+    sum._value = _value + obj._value;
+}
+
+Fixed& Fixed::operator-(const Fixed& obj) const
+{
+    Fixed subtract;
+    subtract._value = _value - obj._value;
+}
+
+Fixed& Fixed::operator*(const Fixed& obj) const
+{
+    Fixed mult;
+    mult._value = _value * obj._value;
+}
+
+Fixed& Fixed::operator/(const Fixed& obj) const
+{
+    Fixed div;
+    if (_value == 0)
+    std::cerr << "Cannot divide by 0" << std::endl;
+    div._value = _value / obj._value;
+}
+
+
+//I/D operators
+Fixed&	Fixed::operator++()
+{
+    this->_value++;
+    return *this;
+}
+
+Fixed	Fixed::operator++(int)
+{
+    Fixed temp(*this);
+    this->_value++;
+    return (temp);
+}
+
+Fixed&	Fixed::operator--()
+{
+    this->_value--;
+    return *this;
+}
+
+Fixed	Fixed::operator--(int)
+{
+    Fixed temp(*this);
+    this->_value--;
+    return (temp);
+}
+
+
+//rest
+Fixed& Fixed::min(Fixed& a, Fixed& b)
+{
+    return (a < b) ? a : b;
+}
+Fixed& Fixed::max(Fixed& a, Fixed& b)
+{
+    return (a > b) ? a : b;
+}
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
+{
+    return (a < b) ? a : b;
+}
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b)
+{
+    return (a > b) ? a : b;
+}
--- CPP1B/CPP02/ex02/Fixed.hpp
+++ CPP1B/CPP02/ex02/Fixed.hpp
@@ -6,7 +6,49 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 11:41:16 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/05/26 11:41:18 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/05/31 15:15:23 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <cmath>
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int _value;
+		static const int fract_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed& obj);
+		Fixed& operator=(const Fixed& obj);
+		~Fixed();
+		
+		//Comparison operators
+		bool	operator>(const Fixed& obj) const;
+		bool	operator<(const Fixed& obj) const;
+		bool	operator>=(const Fixed& obj) const;
+		bool	operator<=(const Fixed& obj) const;
+		bool	operator==(const Fixed& obj) const;
+		bool	operator!=(const Fixed& obj) const;
+		
+		//Arithmetic operators
+		Fixed&	operator+(const Fixed& obj) const;
+		Fixed&	operator-(const Fixed& obj) const;
+		Fixed&	operator*(const Fixed& obj) const;
+		Fixed&	operator/(const Fixed& obj) const;
+		
+		//I/D operators
+		Fixed&	operator++();
+		Fixed	operator++(int);
+		Fixed&	operator--();
+		Fixed	operator--(int);
+
+		//rest
+		static 	Fixed& min(Fixed& a, Fixed& b);
+		static 	Fixed& max(Fixed& a, Fixed& b);
+		static 	const Fixed& min(const Fixed& a, const Fixed& b);
+		static 	const Fixed& max(const Fixed& a, const Fixed& b);
+};
