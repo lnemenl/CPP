60acd0d5c9e29118ac5e7193a57d10e865042a30
Ruslan <ruslankhakimullin@gmail.com>
2025-06-02 11:05:47 +0300

    WIP CPP03/ex00

--- CPP1B/CPP03/ex00/ClapTrap.cpp
+++ CPP1B/CPP03/ex00/ClapTrap.cpp
@@ -6,7 +6,42 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 10:11:52 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/02 10:11:53 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/02 11:05:07 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : name(), hit_points(10), energy_points(10), attack_damage(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string& name) : name(name), hit_points(10), energy_points(10), attack_damage(0)
+{
+	std::cout << "Constructor with name called" << std::endl;
+}
+
+ClapTrap::ClapTrap (const ClapTrap& obj)
+	: name(obj.name), hit_points(obj.hit_points), energy_points(obj.energy_points), attack_damage(obj.attack_damage)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &obj)
+	{
+		this->name = obj.name;
+		hit_points = obj.hit_points;
+		energy_points = obj.energy_points;
+		attack_damage = obj.attack_damage;
+	}
+	return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- CPP1B/CPP03/ex00/ClapTrap.hpp
+++ CPP1B/CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,28 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 10:11:55 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/02 10:11:56 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/02 10:45:55 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string		name;
+		unsigned int	hit_points;
+		unsigned int	energy_points;
+		unsigned int	attack_damage;
+		
+	public:
+		ClapTrap();
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap& obj);
+		ClapTrap& operator=(const ClapTrap&obj);
+		~ClapTrap();
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
