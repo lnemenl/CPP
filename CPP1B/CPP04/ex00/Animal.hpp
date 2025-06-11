d635e7220088b724da8e0e05dc15e9d618bbb70e
Ruslan <ruslankhakimullin@gmail.com>
2025-06-11 18:29:49 +0300

    WIP CPP04/ex00

---
+++ CPP1B/CPP04/ex00/Animal.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 18:18:38 by rkhakimu          #+#    #+#             */
+/*   Updated: 2025/06/11 18:29:27 by rkhakimu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+
+#include <iostream>
+
+class Animal
+{
+    protected:
+        std::string type;
+        
+    public:
+        Animal();
+        Animal(const Animal& obj);
+        Animal& operator=(const Animal& obj);
+        virtual ~Animal();
+
+        virtual void makeSound();
+        void getType(void);
+        
+        
+};
+
+#endif
+
