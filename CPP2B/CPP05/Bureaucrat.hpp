64e3f7c0f95559d61b7c3354d4e9215ea058f568
Ruslan <ruslankhakimullin@gmail.com>
2025-06-17 10:22:06 +0300

    CPP05/ex00 WIP added methods and exception classes

--- CPP2B/CPP05/Bureaucrat.hpp
+++ CPP2B/CPP05/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:29:21 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/16 11:52:30 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/17 10:20:59 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,41 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <exception>
 
 class Bureaucrat
 {
 	private:
-		std::string name;
+		const std::string name;
+		int grade;
+		
 	public:
 		Bureaucrat();
-		Bureaucrat(std::string name);
+		Bureaucrat(const std::string name, int grade);
 		Bureaucrat(const Bureaucrat& obj);
 		Bureaucrat& operator=(const Bureaucrat& obj);
 		~Bureaucrat();
 		
 		const std::string& getName() const;
 		void getGrade() const;
+
+		void incrementGrade();
+		void DecrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const noexcept override;
+		};
 		
-		
-}
+};
+	
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
 
 #endif
-
