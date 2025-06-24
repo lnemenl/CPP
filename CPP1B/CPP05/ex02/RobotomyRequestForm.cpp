9d019adeb34dcb1a14317d439ce3974daceea66f
Ruslan <ruslankhakimullin@gmail.com>
2025-06-24 15:44:57 +0300

    Robotomy part WIP. Tests needed

--- CPP1B/CPP05/ex02/RobotomyRequestForm.cpp
+++ CPP1B/CPP05/ex02/RobotomyRequestForm.cpp
@@ -6,9 +6,34 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 12:02:13 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/24 14:40:10 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/24 15:44:31 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
+#include <cstdlib> // For rand(), srand()
+#include <ctime> // For time()
 
+RobotomyRequestForm::RobotomyRequestForm(const std::string& target)
+    :AForm("RobotomyRequestForm", 72, 45), _target(target) {}
+
+void RobotomyRequestForm::execute(Bureaucrat const& executor) const
+{
+    checkExecution(executor);
+
+    std::cout << "Drilling noises" << std::endl;
+
+    // Setting the starting poing for sequence only the first time when execute() is called, so that not to get the same sequence every run.
+    // Creates a variable that is shared across all calls to
+    static bool seeded = false;
+    if (!seeded)
+    {
+        std::srand(std::time(0));
+        seeded = true;
+    }
+    
+    if (std::rand() % 2 == 0)
+        std::cout << _target << " has been robotomized successfully." << std::endl;
+    else
+        std::cout << "Robotomy of " << _target << " faield." <<std::endl;
+}
--- CPP1B/CPP05/ex02/ShrubberyCreationForm.hpp
+++ CPP1B/CPP05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,27 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 12:02:47 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/24 12:02:48 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/24 15:31:39 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SHRUBBERYCREATIONFORM_HPP
+#define SHRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+    private:
+        //const because a form's target shouldn't change after creation
+        const std::string _target;
+    public:
+        ShrubberyCreationForm(const std::string& target);
+        ShrubberyCreationForm(const ShrubberyCreationForm&) = default;
+        ShrubberyCreationForm& operator=(const ShrubberyCreationForm&) = delete;
+        ~ShrubberyCreationForm() = default;
+        ShrubberyCreationForm() = delete;
+        void execute(Bureaucrat const& executor) const override;
+};
+
+#endif
