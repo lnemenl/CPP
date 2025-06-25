8800729c8aed5a307f693aa73e4680f24208df53
R <ruslankhakimullin@gmail.com>
2025-06-25 21:58:48 +0300

    CPP05/ex03 intern cpp file done

--- CPP1B/CPP05/ex03/Intern.cpp
+++ CPP1B/CPP05/ex03/Intern.cpp
@@ -6,8 +6,64 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 17:48:16 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/25 17:53:14 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/25 21:58:08 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+
+const char* Intern::FormNotFoundException::what() const noexcept
+{
+	return "Form not found";
+}
+
+static AForm* createShrubbery(const std::string& t)
+{
+	return new ShrubberyCreationForm(t);
+}
+
+static AForm* createRobotomy(const std::string& t)
+{
+	return new RobotomyRequestForm(t);
+}
+
+static AForm* createPardon(const std::string& t)
+{
+	return new PresidentialPardonForm(t);
+}
+
+AForm* Intern::makeForm(const std::string& formName, const std::string& target) const
+{
+	// Using arrays to map form names to their creation functions.
+	const std::string names[] =
+	{
+		"shrubbery creation",
+		"robotomy request",
+		"presidential pardon"
+	};
+
+	// This is an array of function pointers (array of pointers to functions)
+	// Each pointer points to a function that takes a const std::string& (the target)
+	// and returns a pointer to AForm.
+	AForm* (*creators[])(const std::string&) =
+	{
+		createShrubbery,
+		createRobotomy,
+		createPardon
+	};
+	
+	for (int i = 0; i < 3; i++)
+	{
+		if (formName == names[i])
+		{
+			std::cout << "Intern creates " << formName << " form." << std::endl;
+			return creators[i](target);
+		}
+	}
+	
+	throw FormNotFoundException();
+}
--- CPP1B/CPP05/ex03/main.cpp
+++ CPP1B/CPP05/ex03/main.cpp
@@ -6,68 +6,71 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:29:32 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/25 17:40:43 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/06/25 21:55:46 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Bureaucrat.hpp"
+#include "Intern.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 
 int main()
 {
-	std::cout << "=== Bureaucrat and Form Demonstration ===\n" << std::endl;
+    std::cout << "=== Intern, Bureaucrat, and Form Demonstration ===\n" << std::endl;
 
-	try
-	{
-		Bureaucrat ONE("ONE", 1);
-		Bureaucrat TWO("TWO", 140);
-		Bureaucrat THREE("THREE", 50);
+    try
+    {
+        Bureaucrat ONE("ONE", 1);
+        Bureaucrat TWO("TWO", 140);
 
-		ShrubberyCreationForm shrub("garden");
-		RobotomyRequestForm robot("RD2D");
-		PresidentialPardonForm pardon("LORD BECKETT");
+        Intern someRandomIntern;
 
-		std::cout << "\n--- TWO tries to sign and execute ShrubberyCreationForm ---" << std::endl;
-		std::cout << "TWO tries to sign (should succeed):" << std::endl;
-		TWO.signForm(shrub);
-		std::cout << "TWO tries to execute (should fail: grade too low):" << std::endl;
-		TWO.executeForm(shrub);
+        std::cout << "\n--- Intern creates forms ---" << std::endl;
+        AForm* shrub = someRandomIntern.makeForm("shrubbery creation", "Tree");
+        AForm* robot = someRandomIntern.makeForm("robotomy request", "RD2D");
+        AForm* pardon = someRandomIntern.makeForm("presidential pardon", "LORD BECKETT");
 
-		std::cout << "\n--- ONE signs and executes RobotomyRequestForm ---" << std::endl;
-		ONE.signForm(robot);
-		std::cout << "\n--- ONE signs and executes RobotomyRequestForm multiple times (should see random results) ---" << std::endl;
-		ONE.signForm(robot);
-		for (int i = 0; i < 5; ++i)
+        std::cout << "\n--- TWO tries to sign and execute ShrubberyCreationForm ---" << std::endl;
+        TWO.signForm(*shrub);
+        TWO.executeForm(*shrub);
+
+        std::cout << "\n--- ONE signs and executes all forms ---" << std::endl;
+        ONE.signForm(*shrub);
+        ONE.executeForm(*shrub);
+
+        ONE.signForm(*robot);
+        for (int i = 0; i < 3; ++i)
 		{
-			std::cout << "Attempt " << (i + 1) << ": ";
-			ONE.executeForm(robot);
-		}
-		ONE.executeForm(robot);
+            std::cout << "Robotomy attempt " << (i + 1) << ": ";
+            ONE.executeForm(*robot);
+        }
 
-		std::cout << "\n--- ONE signs and executes PresidentialPardonForm ---" << std::endl;
-		ONE.signForm(pardon);
-		ONE.executeForm(pardon);
+        ONE.signForm(*pardon);
+        ONE.executeForm(*pardon);
 
-		std::cout << "\n--- THREE tries to sign and execute RobotomyRequestForm ---" << std::endl;
-		THREE.signForm(robot);   // Should fail
-		THREE.executeForm(robot);
+        std::cout << "\n--- Intern tries to create an unknown form ---" << std::endl;
+        try
+		{
+            AForm* unknown = someRandomIntern.makeForm("unknown form", "Nobody");
+            delete unknown;
+        }
+		catch (const std::exception& e)
+		{
+            std::cerr << "Caught exception: " << e.what() << std::endl;
+        }
 
-		std::cout << "\n--- ONE executes ShrubberyCreationForm (should succeed) ---" << std::endl;
-		ONE.executeForm(shrub);
+        delete shrub;
+        delete robot;
+        delete pardon;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception caught in main: " << e.what() << std::endl;
+    }
 
-		std::cout << "\n--- ONE tries to execute an unsigned ShrubberyCreationForm ---" << std::endl;
-		ShrubberyCreationForm notSigned("yard");
-		ONE.executeForm(notSigned);
-
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception caught in main: " << e.what() << std::endl;
-	}
-
-	std::cout << "\n=== End of Demonstration ===" << std::endl;
-	return 0;
+    std::cout << "\n=== End of Demonstration ===" << std::endl;
+    return 0;
 }
