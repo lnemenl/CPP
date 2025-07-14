3c2a31a4f20a8326a031e8a56b7c445467e06f25
R <ruslankhakimullin@gmail.com>
2025-07-14 22:25:07 +0300

    WIP CPP06ex00

--- CPP1B/CPP06/ex00/ScalarConverter.cpp
+++ CPP1B/CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,110 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 15:03:17 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/26 15:03:18 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/07/14 22:24:42 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+#include <cmath>
+#include <cctype>
+#include <charconv>
+//c_cpp_properties.json
+
+
+bool    ScalarConverter::isCharLiteral(std::string_view literal)
+{
+	return literal.size() == 3 && literal[0] == '\'' && literal[2] == '\'' && std::isprint(literal[1]);
+}
+
+bool    ScalarConverter::isPseudoLiteral(std::string_view literal)
+{
+	return	literal == "-inf" || literal == "+inf" || literal == "nan" ||
+			literal == "-inff" || literal == "+inff" || literal == "nanf";
+}
+
+bool	ScalarConverter::isIntLiteral(std::string_view literal)
+{
+	if (literal.empty()) return false;
+	size_t start = (literal[0] == '+' || literal[0] == '-') ? 1 : 0;
+	if (start == literal.size()) return false;
+	
+	for (size_t i = start; i < literal.size(); ++i)
+	{
+		if (!std::isdigit(literal[i])) return false;
+	}
+	return true;
+}
+
+bool ScalarConverter::isFloatLiteral(std::string_view literal)
+{
+	if (literal.empty() || literal.back() != 'f') return false;
+	std::string_view num = literal.substr(0, literal.size() - 1);
+	if (num.empty()) return false;
+	size_t start = (num[0] == '+' || num[0] == '-') ? 1 : 0;
+	bool hasDot = false;
+	for (size_t i = start; i < num.size(); ++i)
+	{
+		if (num[i] == '.')
+		{
+			if (hasDot) return false;
+			hasDot = true;
+		}
+		else if (!std::isdigit(num[i]))
+		{
+			return false;
+		}
+	}
+	return hasDot;
+}
+
+bool ScalarConverter::isDoubleLiteral(std::string_view literal)
+{
+    if (literal.empty()) return false;
+    size_t start = (literal[0] == '+' || literal[0] == '-') ? 1 : 0;
+    if (start == literal.size()) return false;
+    bool hasDot = false;
+    for (size_t i = start; i < literal.size(); ++i)
+	{
+        if (literal[i] == '.')
+		{
+            if (hasDot) return false;
+            hasDot = true;
+        }
+		else if (!std::isdigit(literal[i]))
+		{
+            return false;
+        }
+    }
+    return hasDot;
+}
+
+ScalarConverter::LiteralType ScalarConverter::detectType(std::string_view literal)
+{
+	if (literal.empty())			return LiteralType::INVALID;
+	if (isPseudoLiteral(literal))	return LiteralType::PSEUDO;
+	if (isCharLiteral(literal))		return LiteralType::CHAR;
+	if (isFloatLiteral(literal))	return LiteralType::FLOAT;
+	if (isDoubleLiteral(literal))	return LiteralType::DOUBLE;
+	if (isIntLiteral(literal))		return LiteralType::INT;
+									return LiteralType::INVALID;
+}
+
+void	ScalarConverter::toChar(std::string_view literal, ConversionResult& result)
+{
+	result.charValue = literal[1];
+	result.isCharValid = std::isprint(result.charValue);
+}
+
+void	ScalarConverter::toInt(std::string_view literal, ConversionResult& result)
+{
+    int value;
+    auto [ptr, ec] = std::from_chars(literal.data(), literal.data() + literal.size(), value);
+    if (ec == std::errc() && ptr == literal.data() + literal.size())
+	{
+        result.intValue = value;
+        result.isIntValid = true;
+	}
+}
+
 
--- CPP1B/CPP06/ex00/ScalarConverter.hpp
+++ CPP1B/CPP06/ex00/ScalarConverter.hpp
@@ -6,24 +6,84 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 15:03:05 by rkhakimu          #+#    #+#             */
-/*   Updated: 2025/06/26 17:33:09 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2025/07/14 20:15:47 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include "ScalarConverter.hpp"
 #include <iostream>
+#include <string_view>
 #include <limits>
-#include <cmath>
 
 class ScalarConverter
 {
-	public:
-		ScalarConverter() = delete;
-		ScalarConverter(const ScalarConverter&) = delete;
-		ScalarConverter& operator=(const ScalarConverter&) = delete;
-		~ScalarConverter() = default;
+	private:
+		ScalarConverter() 									= delete;
+		ScalarConverter(const ScalarConverter&)				= delete;
+		ScalarConverter& operator=(const ScalarConverter&)	= delete;
+		~ScalarConverter()									= delete;
 		
-		static void convert(const std::string& input);
+		enum class LiteralType
+		{
+			CHAR,
+			INT,
+			FLOAT,
+			DOUBLE,
+			PSEUDO,
+			INVALID
+		};
+		
+		struct ConversionResult
+		{
+			char			charValue{0};
+			int				intValue{0};
+			float			floatValue{0.0f};
+			double			doubleValue{0.0};
+			bool			isCharValid{false};
+			bool			isIntValid{false};
+			bool			isPseudo{false};
+			std::string		pseudoLiteral;
+		};
+		
+		// Type detection
+		static bool			isCharLiteral(std::string_view literal);
+		static bool			isIntLiteral(std::string_view literal);
+		static bool			isFloatLiteral(std::string_view literal);
+		static bool			isDoubleLiteral(std::string_view literal);
+		static bool			isPseudoLiteral(std::string_view literal);
+		static LiteralType	detectType(std::string_view literal);
+
+		// Converting to actual type
+		static void			toChar(std::string_view literal, ConversionResult& result);
+    	static void			toInt(std::string_view literal, ConversionResult& result);
+    	static void			toFloat(std::string_view literal, ConversionResult& result);
+    	static void			toDouble(std::string_view literal, ConversionResult& result);
+    	static void			toPseudo(std::string_view literal, ConversionResult& result);
+
+		// Convert to other types
+		static void			toCharFromInt(const ConversionResult& src, ConversionResult& dest);
+    	static void			toCharFromFloat(const ConversionResult& src, ConversionResult& dest);
+    	static void			toCharFromDouble(const ConversionResult& src, ConversionResult& dest);
+    	static void			toIntFromChar(const ConversionResult& src, ConversionResult& dest);
+    	static void			toIntFromFloat(const ConversionResult& src, ConversionResult& dest);
+    	static void			toIntFromDouble(const ConversionResult& src, ConversionResult& dest);
+    	static void			toFloatFromChar(const ConversionResult& src, ConversionResult& dest);
+    	static void			toFloatFromInt(const ConversionResult& src, ConversionResult& dest);
+    	static void			toFloatFromDouble(const ConversionResult& src, ConversionResult& dest);
+    	static void			toDoubleFromChar(const ConversionResult& src, ConversionResult& dest);
+    	static void			toDoubleFromInt(const ConversionResult& src, ConversionResult& dest);
+    	static void			toDoubleFromFloat(const ConversionResult& src, ConversionResult& dest);
+		
+		// Displaying the results
+		static void			displayChar(const ConversionResult& result);
+    	static void			displayInt(const ConversionResult& result);
+    	static void			displayFloat(const ConversionResult& result);
+    	static void			displayDouble(const ConversionResult& result);
+
+		public:
+			static void convert(const std::string& literal);
 };
 
 // A static method in a class is a function that:
