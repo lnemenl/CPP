e48417b305192045e548500dc3be003ce6f99fc5
Ruslan <ruslankhakimullin@gmail.com>
2025-08-28 15:47:50 +0300

    CPP08 WIP/ ex02. When testing with project main I could not compile it. So Found a great fix for accessing protected c. Need to add more tests

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -11,21 +11,22 @@ class MutantStack : public std::stack<T, Container>
         using const_iterator = typename Container::const_iterator;
         using reverse_iterator = typename Container::reverse_iterator;
         using const_reverse_iterator = typename Container::const_reverse_iterator;
+        using std::stack<T, Container>::c;
 
         MutantStack() = default;
         ~MutantStack() = default;
         MutantStack(const MutantStack&) = default;
         MutantStack& operator=(const MutantStack&) = default;
 
-        iterator begin() { return <Container&>(*this).begin(); }
-        iterator end() { return <Container&>(*this).end(); }
+        iterator begin() { return c.begin(); }
+        iterator end() { return c.end(); }
 
-        const_iterator begin() const { return <Container&>(*this).begin(); }
-        const_iterator end() const { return <Container&>(*this).end(); }
+        const_iterator begin() const { return c.begin(); }
+        const_iterator end() const { return c.end(); }
 
-        reverse_iterator rbegin() { return <Container&>(*this).rbegin(); }
-        reverse_iterator rend() { return <Container&>(*this).rend(); }
+        reverse_iterator rbegin() { return c.rbegin(); }
+        reverse_iterator rend() { return c.rend(); }
 
-        const_reverse_iterator rbegin() const { return <Container&>(*this).rbegin(); }
-        const_reverse_iterator rend() const { return <Container&>(*this).rend(); }
+        const_reverse_iterator rbegin() const { return c.rbegin(); }
+        const_reverse_iterator rend() const { return c.rend(); }
 };
