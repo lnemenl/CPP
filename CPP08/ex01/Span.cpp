59b3cce9d59a32ab01fd0dadc2e8e93b9876c2ae
Ruslan <ruslankhakimullin@gmail.com>
2025-08-28 08:45:12 +0300

    CPP08 WIP ex01/ shortestSpan returned to original, trying to optimize and to add additional checks for overflows

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -20,18 +20,18 @@ unsigned int Span::longestSpan()
     return (*result.second - *result.first);
 }
 
-unsigned int Span::shortestSpan()
-{
-    if (_numbers.size() <= 1)
-        throw NotEnoughNumbersForSpan();
+// unsigned int Span::shortestSpan()
+// {
+//     if (_numbers.size() <= 1)
+//         throw NotEnoughNumbersForSpan();
 
-    std::vector<int> sortedrvec = _numbers;
-    std::sort(sortedrvec.begin(), sortedrvec.end());
-    std::vector<int> differences(sortedrvec.size());
-    std::adjacent_difference(sortedrvec.begin(), sortedrvec.end(), differences.begin());
-    auto result = std::min_element(differences.begin() + 1, differences.end());
-    return (*result);
-}
+//     std::vector<int> sortedrvec = _numbers;
+//     std::sort(sortedrvec.begin(), sortedrvec.end());
+//     std::vector<int> differences(sortedrvec.size());
+//     std::adjacent_difference(sortedrvec.begin(), sortedrvec.end(), differences.begin());
+//     auto result = std::min_element(differences.begin() + 1, differences.end());
+//     return (*result);
+// }
 
 
 const char* Span::SpanIsFull::what() const noexcept
@@ -42,4 +42,24 @@ const char* Span::SpanIsFull::what() const noexcept
 const char* Span::NotEnoughNumbersForSpan::what() const noexcept
 {
     return "Not enough numbers to perform a Span :(";
+}
+
+
+unsigned int Span::shortestSpan()
+{
+    if (_numbers.size() <= 1)
+        throw NotEnoughNumbersForSpan();
+
+    std::vector<int> sortedrvec = _numbers;
+    std::sort(sortedrvec.begin(), sortedrvec.end());
+    unsigned int minSpan = sortedrvec[1] - sortedrvec[0];
+    for (size_t i = 2; i < sortedrvec.size(); ++i)
+    {
+        unsigned int currentSpan = sortedrvec[i] - sortedrvec[i - 1];
+        if (currentSpan < minSpan)
+        {
+            minSpan = currentSpan;
+        }
+    }
+    return minSpan;
 }
